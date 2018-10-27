41870c7a5cdad52a88144d2814c0f5fdf56eb381
AndrewYr <andrewyr93@gmail.com>
2018-10-27 15:13:33 +0300

    ДЗ 1

---
+++ work1/work9.py
@@ -0,0 +1,16 @@
+#9 Вводятся три разных числа. Найти, какое из них является средним (больше одного, но меньше другого).
+a = int(input('Первое число: '))
+b = int(input('Второе число: '))
+c = int(input('Третье число: '))
+if (a<b and b<c):
+    print(b)
+elif (c<b and b<a):
+    print(b)
+elif (a<c and c<b):
+    print(c)
+elif (b<c and c<a):
+    print(c)
+elif (c<a and a<b):
+    print(a)
+elif (b<a and a<c):
+    print(a)
